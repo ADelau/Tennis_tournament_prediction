012d7e5202c61552db690a9227b99f5526262d6e
François Cornet <f.cornet@student.uliege.be>
2019-05-09 10:54:59 +0200

    reorganisation

--- db/last_matches.sql
+++
@@ -1,8 +0,0 @@
---X last match of 1 player
-SELECT *
-FROM 
-(SELECT * 
-FROM matches 
-WHERE player_1=200282 OR player_2=200282
-ORDER BY date DESC)
-LIMIT 2;
--- db/previous_matches_same_players.sql
+++
@@ -1,4 +0,0 @@
---previous matches between 2 opponents
-SELECT * 
-FROM matches 
-WHERE (player_1= 104745 AND player_2 = 104925) OR (player_1=104925  AND player_2 = 104745);
