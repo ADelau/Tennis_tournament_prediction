d8d88269a17c6d5b95abb1ac1a35490c7c04aab9
François Cornet <f.cornet@student.uliege.be>
2019-05-09 11:23:27 +0200

    reorganisation

--- simulations/tournament.py
+++ simulations/tournament.py
@@ -2,24 +2,104 @@ from data import load_from_csv, load_dataset, load_train_set, encode
 import pandas as pd 
 import math
 import random
+import math
+import numpy as np
+
+PLAYER_NOT_PARTICIPATING = -1
 
 NB_PLAYERS = 128
 TOT_NB_ROUNDS = int(math.log(NB_PLAYERS, 2)) + 1
 random.seed(None)
 
+def load_player_matrix(fileName):
+	"""
+	Load the player matrix
+
+	Parameters
+    ----------
+    fileName: The name of the file containing the player matrix
+    
+    Returns
+    -------
+    The player matrix, a dictionary of dictionary of player names containg the
+    probability that the first index player wins, the rank of the first index
+    player, the rank of the second index player, the atp points of the first
+    index player and the atp points of the second index player.
+	"""
+
+	playerMatrixDF = load_from_csv(fileName)
+	playerMatrix = {}
+
+	for index, row in playerMatrixDF.iterrows():
+		playerMatrix[(row["name_1"], row["name_2"])] = (row["proba_1"], 
+														  row["rank_1"], 
+														  row["rank_2"],
+														  row["rank_points_1"],
+														  row["rank_points_2"])
+
+	return playerMatrix
+
+def load_players(playerMatrix):
+	"""
+	Load the player list from a player matrix
+
+	Parameters
+    ----------
+    playerMatrix: The player matrix
+    
+    Returns
+    -------
+    A list of players Objects.
+	"""
+
+	playersSet = set()
+
+	for key in playerMatrix.keys():
+		for playerName in key:
+			if playerName not in playersSet:
+				playersSet.add(playerName)
+
+	players = []
+	id = 0
+
+	for playerName in playersSet:
+		for playerName2 in playersSet:
+
+			try:
+				atpRank = playerMatrix[(playerName, playerName2)][1]
+				atpPoints = playerMatrix[(playerName, playerName2)][3]
+				players.append(Player(id, playerName, atpRank, atpPoints))
+				id += 1
+				break
+			
+			except Exception as e:
+				print(e)
+
+	return players
+
 class Player():
-	def __init__(self, id, name, atpRank):
+	def __init__(self, id, name, atpRank, atpPoints):
 		self.id = id
 		self.name = name
 		self.atpRank = atpRank
+		self.atpPoints = atpPoints
 		self.counterRoundReached = [0]*TOT_NB_ROUNDS # index  nbRound
 		self.counterPlayed = 0
 		self.lastEncounteredRank = None
+		self.qualified = False
 
 	def update(self, roundReached):
 		"""
-		update all round < roundReached as reached and the others as not reached.
+		Update all round < roundReached as reached and the others as not 
+		reached.
+
+		Parameters
+	    ----------
+	    roundReached: The round reached at a simulation
 		"""
+
+		if roundReached == PLAYER_NOT_PARTICIPATING:
+			return
 
 		self.counterPlayed += 1
 		for i in range(roundReached + 1):
@@ -27,7 +107,12 @@ class Player():
 
 	def proba_round_reached(self):
 		"""
-		return list containing proba of reaching the round of the index
+		Get the probabilities of the player to reach each round.
+
+	    Returns
+	    -------
+	    A list of probabilities, the first index correspond to round 128, second
+	    to round 64, ...
 		"""
 
 		return [x/self.counterPlayed for x in self.counterRoundReached]
@@ -39,6 +124,15 @@ class Player():
 		return str(self.name)
 
 def encountered(playerA, playerB):
+	"""
+	Update the players to indicate their last encoutered each other.
+
+	Parameters
+    ----------
+    playerA: The first player
+    playerB: The second player
+	"""
+
 	playerA.lastEncounteredRank = playerB.atpRank
 	playerB.lastEncounteredRank = playerA.atpRank
 
@@ -49,22 +143,43 @@ class Match():
 		self.playerB = playerB
 
 	def update_players(self, playerA, playerB):
+		"""
+		Set the player participating to the match
+
+		Parameters
+	    ----------
+	    playerA: The first player
+	    playerB: The second player
+		"""
+
 		self.playerA = playerA
 		self.playerB = playerB
 
 	def winner(self):
-		probaA = self.self.playerMatrix[(self.playerA.name, self.playerB.name)]
+		"""
+		Draw a winner of the match according to winning probability
+
+		Returns
+	    -------
+	    The player that won the match
+		"""
+
+		probaA = self._get_proba()
 
 		if random.uniform(0, 1) <= probaA:
 			return self.playerA
 		return self.playerB
 
-	def _contruct_input_vector(self):
+	def _get_proba(self):
 		"""
-		generate the vector of features used to predict the outcome
+		Compute the probability of playerA winning against playerB
+
+		Returns
+	    -------
+	    The probability fo playerA winning against playerB
 		"""
 
-		return self.playerMatrix[(self.playerA.name, self.playerB.name)]
+		return self.playerMatrix[(self.playerA.name, self.playerB.name)][0]
 
 	def __repr__(self):
 		return("({}, {})".format(self.playerA.name, self.playerB.name))
@@ -78,20 +193,38 @@ class Round():
 
 	def next_round(self, matches = None, debug = False):
 		"""
-		return a round resulting from the current one, matches are the matches of the next round 
-		can only be None if last round
+		Return a round resulting from a simulation of the current one.
+
+		Parameters
+	    ----------
+	    matches: The matches of the next round, can only be None if it is the 
+	    		 last round
+	    
+	    Returns
+	    -------
+	    A Round object corresponding to the resulting next round
 		"""
 
 		nextPlayers = self.simulate(debug)
-		nextRound = Round(self.roundNb + 1, nextPlayers, matches, self.prevRounds + [self])
+		nextRound = Round(self.roundNb + 1, nextPlayers, matches, 
+						  self.prevRounds + [self])
 
 		return nextRound
 
 	def simulate(self, debug = False):
 		"""
-		construct a tree from players, simulated matches and returns a list of players that passed
-		"""
+		construct a matching from current players, simulate matches and returns 
+		a list of players that passed the round.
 
+		Parameters
+	    ----------
+		debug: Boolean indicating of it must be executed in debug mode.
+	    
+	    Returns
+	    -------
+	    A list of players that passed the round simulation.
+		"""
+		
 		# roundNb starts at 0, this is for the 2 first rounds
 		if self.roundNb == 0:
 			self._initialisation_matching()
@@ -120,21 +253,27 @@ class Round():
 
 	def _initialisation_matching(self):
 		"""
-		Updates matches accord to the rules of the first round. 32 best one player out of 4.
+		Assign the players to the matches according to the rules of the first
+		round.
 		"""
+
 		# Order players, better ranked first
 		orderedPlayers = sorted(self.players)
 		
-		firstPlayers = random.sample(orderedPlayers[:32], len(orderedPlayers[:32]))
-		otherPlayers = random.sample(orderedPlayers[32:], len(orderedPlayers[32:]))
+		firstPlayers = random.sample(orderedPlayers[:32], 
+									 len(orderedPlayers[:32]))
+		otherPlayers = random.sample(orderedPlayers[32:], 
+									 len(orderedPlayers[32:]))
 
-		for evenMatch, A, B in zip(self.matches[0::2], firstPlayers, otherPlayers[:32]):
+		for evenMatch, A, B in zip(self.matches[0::2], firstPlayers, 
+								   otherPlayers[:32]):
 			evenMatch.playerA = A
 			evenMatch.playerB = B
 
 			encountered(A, B)
 
-		for oddMatch, A, B in zip(self.matches[1::2], otherPlayers[32:64], otherPlayers[64:]):
+		for oddMatch, A, B in zip(self.matches[1::2], otherPlayers[32:64], 
+								  otherPlayers[64:]):
 			oddMatch.playerA = A
 			oddMatch.playerB = B
 
@@ -142,11 +281,13 @@ class Round():
 
 	def _early_matching(self):
 		"""
-		Update matches according to rules of the second round, simply propagate the tree
+		Assign the players to the matches according to the rules of the second
+		round, simply propagate the tree.
 		"""
 		
 		# Player are ordered as previous matches
-		for match, playerA, playerB in zip(self.matches, self.players[0::2], self.players[1::2]):
+		for match, playerA, playerB in zip(self.matches, self.players[0::2], 
+										   self.players[1::2]):
 
 			match.playerA = playerA
 			match.playerB = playerB
@@ -155,7 +296,8 @@ class Round():
 
 	def _end_matching(self):
 		"""
-		Updates matches according to rules of the last rounds
+		Assign the players to the matches according to the rules of the last
+		rounds.
 		"""
 		
 		# Sort according to atp rank
@@ -166,31 +308,41 @@ class Round():
 
 		# First part = the one who encountered the strongest in the half best
 		# Second part = the one who encountered the weakest in the half best
-		orderedFirstPlayers = sorted(firstPlayers, key = lambda x: x.lastEncounteredRank)
+		orderedFirstPlayers = sorted(firstPlayers, 
+									 key = lambda x: x.lastEncounteredRank)
 		
 		firstPart = orderedFirstPlayers[:int(len(orderedFirstPlayers)/2)]
 		secondPart = orderedFirstPlayers[int(len(orderedFirstPlayers)/2):]
 
 		# Third part = the half best in the half worst		
-		thirdPart = orderedPlayers[int(len(orderedPlayers)/2): int(3*len(orderedPlayers)/4)]
+		thirdPart = orderedPlayers[int(len(orderedPlayers)/2): 
+								   int(3*len(orderedPlayers)/4)]
 
 		# Fourth part is the half worst in the half worst
 		fourthPart = orderedPlayers[int(3*len(orderedPlayers)/4):]
 
-		# Match the one in first half that encountered strongest against weakest in second half
+		# Match the one in first half that encountered strongest against 
+		# weakest in second half
 		sampleFirstPart = random.sample(firstPart, len(firstPart))
 		samplesecondPart = random.sample(secondPart, len(secondPart))
 		samplethirdPart = random.sample(thirdPart, len(thirdPart))
 		samplefourthPart = random.sample(fourthPart, len(fourthPart))
 
-		for match, playerA, playerB in zip(self.matches[:int(len(self.matches)/2)], sampleFirstPart, samplefourthPart):
+		for match, playerA, playerB in zip(
+				self.matches[:int(len(self.matches)/2)], sampleFirstPart, 
+				samplefourthPart):
+
 			match.playerA = playerA
 			match.playerB = playerB
 
 			encountered(playerA, playerB)
 
-		# Match the one in first half that encountered weakest against strongest in second half
-		for match, playerA, playerB in zip(self.matches[int(len(self.matches)/2):], samplesecondPart, samplethirdPart):
+		# Match the one in first half that encountered weakest against strongest 
+		# in second half
+		for match, playerA, playerB in zip(
+				self.matches[int(len(self.matches)/2):], samplesecondPart, 
+				samplethirdPart):
+		
 			match.playerA = playerA
 			match.playerB = playerB
 
@@ -198,55 +350,52 @@ class Round():
 
 	def _last_round_matching(self):
 		"""
-		Perform matching for the last round
+		Assign the players to the matches according to the rules of the last
+		round.
 		"""
 
 		self.matches[0].playerA = self.players[0]
 		self.matches[0].playerB = self.players[1]
 
 class Tournament():
-	def __init__(self, playerMatrixFile):
+	def __init__(self, playerMatrixFile, playerMatrix = None, players = None, totPlayers = None):
 		"""
-		model is the model used to predict
-		"""
+		Constructor
 
-		self.playerMatrix = self._load_player_matrix(playerMatrixFile)
-		self.players = self._load_players(self.playerMatrix)
-		self.matches = self._load_matches()
-
-	def _load_player_matrix(self, fileName):
-		playerMatrixDF = load_from_csv(fileName)
-		playerMatrix = {}
-
-		for index, row in playerMatrixDF.iterrows():
-			playerMatrix[(row["name_1"], row["name_2"])] = (row["rank_1"], row["rank_2"])
-
-		return playerMatrix
-
-	def _load_players(self, playerMatrix):
-		"""
-		Return a list of Players
+		Parameters
+	    ----------
+		playerMatrixFile: The name of the file containing the playerMatrix
+		playerMarix: The playerMatrix, if passed playerMaxtrixFile won't be used
+		player: The players participating to the tournament, is not passed, the
+				player matrix must be of size 128 and the players will be the 
+				ones in the player matrix.
 		"""
 		
-		playersSet = set()
+		if playerMatrix is None:
+			self.playerMatrix = load_player_matrix(playerMatrixFile)
+		else:
+			self.playerMatrix = playerMatrix
 
-		for key in playerMatrix.keys():
-			for playerName in key:
-				if playerName not in playersSet:
-					playersSet.add(playerName)
+		if players is None:
+			self.players = load_players(self.playerMatrix)
+		else:
+			self.players = players
 
-		players = []
-		id = 0
-		for playerName in playersSet:
-			atpRank = playerMatrix[(playerName, playerName)][0]
-			players.append(Player(id, playerName, atpRank))
-			id += 1
+		if totPlayers is None:
+			self.totPlayers = len(self.players)
+		else:
+			self.totPlayers = totPlayers
 
-		return players
+		self.matches = self._create_matches()
 
-	def _load_matches(self):
+	def _create_matches(self):
 		"""
-		Return a list of list of Matches, first index = round number
+		Create the matches of the tournament
+
+	    Returns
+	    -------
+	    A list of lists of matches. The first index is the round number, the
+	    second index is the match number inside the round.
 		"""
 
 		matches = []
@@ -261,13 +410,29 @@ class Tournament():
 		return matches
 
 	def rollout(self, debug = False):
+		"""
+		Perform a simulation of the tournament and update the results.
+
+		Parameters
+	    ----------
+		debug: Boolean indicating of it must be executed in debug mode.
+		"""
+
 		results = self._simulate_tournament(debug)
 		for player in self.players:
 			player.update(results[player.id])
 
 	def _simulate_tournament(self, debug = False):
 		"""
-		return a list with round reached, index = player ID
+		Perform a simulation of the tournament
+
+		Parameters
+	    ----------
+		debug: Boolean indicating of it must be executed in debug mode.
+	    
+	    Returns
+	    -------
+	    An array of round reached, the index is the internal id of the player.
 		"""
 		
 		rounds = [Round(0, self.players, self.matches[0])]
@@ -277,7 +442,8 @@ class Tournament():
 
 		rounds.append(rounds[-1].next_round(None, debug))
 
-		results = [0] * len(self.players)
+		results = np.full((self.totPlayers,), PLAYER_NOT_PARTICIPATING, 
+						  dtype = np.intc)
 
 		for round in rounds:
 			for player in round.players:
@@ -286,33 +452,65 @@ class Tournament():
 		return results
 
 	def monte_carlo(self, nbSimulations, debug = False):
-		for _ in range(nbSimulations):
+		"""
+		Perform several monte carlo simulations of the tournament.
+
+		Parameters
+	    ----------
+	    nbSimulations: The number of simulations to perform.
+		debug: Boolean indicating of it must be executed in debug mode.
+		"""
+
+		PRINT_EVERY = 1000
+		printed = 0
+		for simulation in range(nbSimulations):
+			if simulation//PRINT_EVERY > printed:
+				print("iteration {}".format(simulation))
+				printed += 1
+
 			self.rollout(debug)
 
+	def print_results(self):
+		"""
+		Print the results of the simulations
+		"""
+
 		for player in self.players:
-			print("name = {}, result = {}".format(player.name, player.proba_round_reached()))
+			print("name = {}, result = {}".format(player.name, 
+												  player.proba_round_reached()))
 
-class TestEstimator():
-	def __init__(self):
-		pass
+	def results_to_csv(self, fileName):
+		"""
+		Write the results in a csv file.
 
-	def predict(self, ranks):
-		rank1 = ranks[0]
-		rank2 = ranks[1]
-		# Proba que 1 gagne = rank2 / (rank1 + rank2) donc si rank 2 bas proba que 1 gagne faible.
-		return rank2 / (rank1 + rank2)
+		Parameters
+	    ----------
+		fileName: the name of the file without the extension.
+		"""
+
+		fileName = fileName + ".csv"
+		df = pd.DataFrame(columns = ("name", "128", "64", "32", "16", "8", "4", 
+									 "2", "1"))
+		
+		for player in self.players:
+			print([round(item, 5) for item in player.proba_round_reached()])
+			df.loc[len(df)] = [player.name] + [round(item, 4) for item in 
+											   player.proba_round_reached()]
+
+		df = df.sort_values(by = ["1", "2", "4", "8", "16", "32", "64", "128"], 
+							ascending = False)
+
+		df.to_csv(fileName, index = False, float_format = "%.4f")
+
 
 if __name__ == "__main__":
-	model = TestEstimator()
-	tournament = Tournament("testMatrix.csv", None, model)
+	tournament = Tournament("probas.csv")
 	for player in tournament.players:
-		print("id = {}, name = {}, atpRank = {}".format(player.id, player.name, player.atpRank))
-
-	print("matches:")
-	for round in tournament.matches:
-		for match in round:
-			print("features = {}".format(match.features))
+		print("id = {}, name = {}, atpRank = {}".format(player.id, player.name, 
+														player.atpRank))
 
 	tournament.monte_carlo(1, True)
-
-	tournament.monte_carlo(500)
+	tournament.print_results()
+	
+	tournament.monte_carlo(100000)
+	tournament.results_to_csv("results_2018")	
